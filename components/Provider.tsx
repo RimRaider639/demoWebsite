c62cf8a1e1ab5b7e7fea6bd5b37d83f87be4da88
RimRaider639 <madhurimapoddar639@gmail.com>
2023-05-05 15:37:58 +0530

    init setup

---
+++ components/Provider.tsx
@@ -0,0 +1,16 @@
+import { CacheProvider } from '@chakra-ui/next-js'
+import { ChakraProvider } from '@chakra-ui/react'
+
+export default function Provider({ 
+    children 
+  }: { 
+  children: React.ReactNode 
+  }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider>
+        {children}
+      </ChakraProvider>
+    </CacheProvider>
+  )
+}
